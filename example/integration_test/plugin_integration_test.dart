7adf274f5773dabb7c7a6177b09b1e795a649ef0
Rexios <rexios@rexios.dev>
2026-01-28 11:38:04 -0500

    Integration test works

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -6,14 +6,53 @@
 // For more information about Flutter integration tests, please see
 // https://flutter.dev/to/integration-testing
 
+import 'dart:isolate';
 
+import 'package:flutter_local_notifications/flutter_local_notifications.dart'
+    hide NotificationDetails;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:foreground_service_isolate/foreground_service_isolate.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  
+  await Permission.notification.request();
+
+  test('Foreground service notification is created', () async {
+    final flnp = FlutterLocalNotificationsPlugin();
+
+    final an1 = await flnp.getActiveNotifications();
+    expect(an1, isEmpty);
+
+    final connection = await spawnForegroundServiceIsolate(
+      entryPoint,
+      notificationDetails: const NotificationDetails(
+        channelId: 'foreground_service_isolate',
+        channelName: 'Foreground Service Isolate',
+        id: 1,
+        contentTitle: 'Foreground Service Isolate',
+        contentText: 'Running...',
+        smallIcon: 'ic_launcher',
+      ),
+    );
+
+    // https://developer.android.com/about/versions/12/behavior-changes-all#foreground-service-notification-delay
+    await Future.delayed(const Duration(seconds: 10));
+
+    final an2 = await flnp.getActiveNotifications();
+    expect(an2, hasLength(1));
+
+    connection.close();
+
+    final an3 = await flnp.getActiveNotifications();
+    expect(an3, isEmpty);
+  });
+}
+
+@pragma('vm:entry-point')
+void entryPoint(SendPort send) {
+  final connection = setupIsolate(send);
 }
