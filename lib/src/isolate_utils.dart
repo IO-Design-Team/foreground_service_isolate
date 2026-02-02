2b3d501495df2947542d8727e96da7c8362c50e8
Rexios <rexios@rexios.dev>
2026-02-02 18:09:17 -0500

    Add back onConnect

--- lib/src/isolate_utils.dart
+++ lib/src/isolate_utils.dart
@@ -1,14 +1,18 @@
+import 'dart:isolate';
+
 import 'package:foreground_service_isolate/foreground_service_isolate.dart';
 
 /// Spawn an isolate in a foreground service
 Future<IsolateConnection> spawnForegroundServiceIsolate(
   IsolateEntryPoint entryPoint, {
+  void Function(SendPort send)? onConnect,
   void Function()? onExit,
   void Function(String error, StackTrace stackTrace)? onError,
   required NotificationDetails notificationDetails,
   Set<ForegroundServiceType> foregroundServiceTypes = const {},
 }) {
   return spawnIsolateConnection(
+    onConnect: onConnect,
     onExit: onExit,
     onError: onError,
     spawn: (send, control) => ForegroundServiceIsolate.spawn(
