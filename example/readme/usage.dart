2b3505e3cd4ccd4b402521f3d6274b13bc384ae3
Rexios <rexios@rexios.dev>
2026-02-20 09:34:37 -0500

    Cleanup

--- example/readme/usage.dart
+++ example/readme/usage.dart
@@ -15,7 +15,6 @@ void example() async {
     notificationDetails: const NotificationDetails(
       channelId: 'foreground_service_isolate',
       channelName: 'Foreground Service Isolate',
-      channelDescription: 'Foreground Service Isolate',
       id: 1,
       contentTitle: 'Foreground Service Isolate',
       contentText: 'Running...',
